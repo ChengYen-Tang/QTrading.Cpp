1cce335b0444d8c160d19cd19263fabd5ccf9d01
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2026-04-06 01:37:45 +0800

    Remove unused fixed universe selector

--- QTrading.Universe/tests/UniverseTests.cpp
+++ QTrading.Universe/tests/UniverseTests.cpp
@@ -1,22 +1,6 @@
-#include "Universe/FixedUniverseSelector.hpp"
+#include "Universe/IUniverseSelector.hpp"
 
 #include <gtest/gtest.h>
-
-TEST(FixedUniverseSelectorTests, ReturnsFixedSymbols)
-{
-    QTrading::Universe::FixedUniverseSelector selector({ "BTCUSDT_SPOT", "BTCUSDT_PERP" });
-    auto sel = selector.select();
-    ASSERT_EQ(sel.universe.size(), 2u);
-    EXPECT_EQ(sel.universe[0], "BTCUSDT_SPOT");
-    EXPECT_EQ(sel.universe[1], "BTCUSDT_PERP");
-}
-
-TEST(FixedUniverseSelectorTests, ReturnsEmptyUniverseWhenSymbolsOmitted)
-{
-    QTrading::Universe::FixedUniverseSelector selector({});
-    auto sel = selector.select();
-    EXPECT_TRUE(sel.universe.empty());
-}
 
 TEST(NullUniverseSelectorTests, ReturnsEmptyUniverse)
 {
